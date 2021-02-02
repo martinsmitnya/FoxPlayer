d5de9b39febe534b343cb35076177f6a9b105a19
Martin Smitnya <smitnyamartin@gmail.com>
2021-02-02 17:41:12 +0100

    fetch started

--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
 'use strict'
+
+//All tracks, favourites
+let defaultplaylists = document.querySelector('.defaultplaylists');
+fetch('http://localhost:3000/playlists')
+.then(result => result.json())
+.then(function (data) {
+  for (let i = 0; i< data.length; i++) {
+    let item = document.createElement('p');
+    item.innerText = data[i].title;
+    addedPlaylistsWrapper.appendChild(item);
+  };
+})
+.catch(err => console.log(err))
